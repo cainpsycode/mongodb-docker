7925e6c59356f2711e202572016f27610e3236ff
cainpsycode <cainpsycode@gmail.com>
2025-04-08 20:06:26 +0500

    Update mongo-admin.sh

--- mongo-admin.sh
+++ mongo-admin.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
-
+# Проверка, задан ли пароль root-пользователя MongoDB
 if [ -z "$MONGO_ROOT_PASSWORD" ]; then
   exit 0
 fi
-
+# Запуск фонового процесса
 (
+  # Ожидание запуска MongoDB
   sleep 5
+  # Команда авторизации
   auth="-u root -p $MONGO_ROOT_PASSWORD"
-  js="if (!db.getUser('root')) { db.createUser({ user: 'root', pwd: '$MONGO_ROOT_PASSWORD', roles: [ 'userAdminAnyDatabase' ] }) }"
-
-  while ! mongo admin --eval "$js" && ! mongo admin $auth --eval "$js"; do
+  # JS-скрипт на создание пользователя root (если его нет)
+  js="if (!db.getUser('root')) {
+    db.createUser({
+      user: 'root',
+      pwd: '$MONGO_ROOT_PASSWORD',
+      roles: [ 'userAdminAnyDatabase' ]
+    });
+  }"
+  # Цикл попыток подключения и выполнения JS-скрипта
+  while true; do
+    mongo admin --eval "$js" && break
+    mongo admin $auth --eval "$js" && break
     sleep 5
   done
 ) &
-
+# Завершение основного процесса
 exit 0
